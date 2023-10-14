c7137b7bc415542ebab9af68f455569cf2fc439d
faysalhosen <faysanhosen7025@gmail.com>
2023-10-14 13:48:17 +0600

    Overall setup

---
+++ src/components/AddCoffe.jsx
@@ -0,0 +1,11 @@
+
+
+const AddCoffe = () => {
+    return (
+        <div>
+            <h1>addcoffe</h1>
+        </div>
+    );
+};
+
+export default AddCoffe;
---
+++ src/components/UpdateCoffe.jsx
@@ -0,0 +1,11 @@
+
+
+const UpdateCoffe = () => {
+    return (
+        <div>
+            <h2>update a copy</h2>
+        </div>
+    );
+};
+
+export default UpdateCoffe;
